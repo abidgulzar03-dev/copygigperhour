13c194ffd4bf633e3482894126f81618a84a904a
abidgulzar03-dev <abidgulzar03@gmail.com>
2026-05-09 01:40:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+  const buttons = document.querySelectorAll(".toggle-btn");
+
+  buttons.forEach((btn) => {
+
+    btn.addEventListener("click", () => {
+
+      buttons.forEach((button) => {
+        button.classList.remove("active");
+      });
+
+      btn.classList.add("active");
+
+    });
+
+  });
